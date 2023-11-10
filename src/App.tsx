64f910e912e83e84fe429caf1fff1ed06e505318
quanghuyhihi <huypqph30308@fpt.edu.vn>
2023-11-10 19:30:23 +0700

    add

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<div className="container">
+  <div className="shape">
+    <div className="image" />
+  </div>
+  <h3>Phạm Quang Huy</h3>
+  <h3 className="title">Web Designer</h3>
+  <p>23 tuổi</p>
+  <p>Hà Nội</p>
+  <p>C,PHP,JS,...</p>
+ 
+</div>
+
+    </>
+  )
+}
+
+export default App
